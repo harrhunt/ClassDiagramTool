5bd1b9957f6a73d891019254bd738e31a2299ef9
harrhunt <harrhunt@isu.edu>
2022-01-31 15:47:28 -0700

    made read file static method

--- src/FileIO.hh
+++ src/FileIO.hh
@@ -1,21 +1,28 @@
 #pragma once
+
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
 
-class FileIO{
+class FileIO {
 private:
-    std::string fileName;
-    std::string fileContent;
+//    std::string fileName;
+//    std::string fileContent;
 
 public:
-    FileIO(std::string fileName);
-    FileIO();
+//    FileIO(std::string fileName);
+//    FileIO();
 
     //Getters
-    std::string getFileValue() const {return fileContent;}
-    std::string getFileName() const {return fileName;}
+//    std::string getFileValue() const {return fileContent;}
+//    std::string getFileName() const {return fileName;}
 
     // Methods
-    void readFile(std::string file);
+    static std::string readFile(std::string fileName) {
+        std::ifstream fileIn(fileName);
+        std::stringstream fileContents;
+        fileContents << fileIn.rdbuf();
+        return fileContents.str();
+    }
 };
