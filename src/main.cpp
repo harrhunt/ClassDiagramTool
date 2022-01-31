2fa48ff287263fe8c52a0e59e02fb85b441a0c87
harrhunt <harrhunt@isu.edu>
2022-01-31 15:47:43 -0700

    class parser testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "FileIO.hh"
+#include "ClassParser.hh"
 
 int main(int argc, char const *argv[]) {
-  /* code */
-  return 0;
+    std::string testString = FileIO::readFile("test.h");
+    ClassParser testClass(testString);
+    testClass.parseClass();
+//  std::cout << testString << std::endl;
+    std::cout << testClass.getMethods().size() << std::endl;
+    for (auto method: testClass.getMethods()) {
+        std::cout << method << std::endl;
+    }
+    return 0;
 }
