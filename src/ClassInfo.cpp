034679997d21fe86ea7cdeef4067bdf9f522df34
harrhunt <harrhunt@isu.edu>
2022-02-02 02:42:03 -0700

    added getters for methods and fields and a pretty print

--- src/ClassInfo.cpp
+++ src/ClassInfo.cpp
@@ -7,22 +7,59 @@
 #include "Method.hh"
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-ClassInfo::ClassInfo(string name){
+ClassInfo::ClassInfo(string name) {
     ClassInfo::name = name;
 }
 
-string ClassInfo::getName(){
+string ClassInfo::getName() {
     return name;
 }
-void ClassInfo::setName(string name){
+
+void ClassInfo::setName(string name) {
     ClassInfo::name = name;
 }
 
-void ClassInfo::addField(Field field){
+void ClassInfo::addField(Field field) {
     fields.push_back(field);
 }
-void ClassInfo::addMethod(Method method){
+
+void ClassInfo::addMethod(Method method) {
     methods.push_back(method);
+}
+
+std::vector<Field> ClassInfo::getFields() {
+    return fields;
+}
+
+std::vector<Method> ClassInfo::getMethods() {
+    return methods;
+}
+
+void ClassInfo::prettyPrint() {
+    std::cout << "==========================" << std::endl;
+    std::cout << getName() << std::endl;
+    std::cout << "==========================" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Methods" << std::endl;
+    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+    for (Method method: getMethods()) {
+        std::cout << method.getScope() << " ";
+        std::cout << method.getType() << " ";
+        std::cout << method.getName() << " ";
+        std::cout << method.getParameters() << std::endl;
+    }
+    std::cout << "``````````````````````````" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Fields" << std::endl;
+    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+    for (Field field: getFields()) {
+        std::cout << field.getScope() << " ";
+        std::cout << field.getType() << " ";
+        std::cout << field.getName() << std::endl;
+    }
+    std::cout << "``````````````````````````" << std::endl;
+    std::cout << std::endl << std::endl;
 }
