cbbe9896345135f99e32ef8fdf8af6f6d48f2f09
harrhunt <harrhunt@isu.edu>
2022-02-05 14:50:35 -0700

    aligned columns for pretty print

--- src/ClassInfo.cpp
+++ src/ClassInfo.cpp
@@ -6,6 +6,7 @@
 #include "Field.hh"
 #include "Method.hh"
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 
@@ -46,20 +47,38 @@ void ClassInfo::prettyPrint() {
     std::cout << std::endl;
     std::cout << "Methods" << std::endl;
     std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+    unsigned long long scopeWidth = 0;
+    unsigned long long typeWidth = 0;
+    unsigned long long nameWidth = 0;
+    unsigned long long parameterWidth = 0;
     for (Method method: getMethods()) {
-        std::cout << method.getScope() << " ";
-        std::cout << method.getType() << " ";
-        std::cout << method.getName() << " ";
-        std::cout << method.getParameters() << std::endl;
+        scopeWidth = scopeWidth < method.getScope().length() ? method.getScope().length() : scopeWidth;
+        typeWidth = typeWidth < method.getType().length() ? method.getType().length() : typeWidth;
+        nameWidth = nameWidth < method.getName().length() ? method.getName().length() : nameWidth;
+        parameterWidth = parameterWidth < method.getParameters().length() ? method.getParameters().length() : parameterWidth;
+    }
+    for (Method method: getMethods()) {
+        std::cout << std::setw(scopeWidth + 2) << std::left << method.getScope() << " ";
+        std::cout << std::setw(typeWidth + 2) << std::left << method.getType() << " ";
+        std::cout << std::setw(nameWidth + 2) << std::left << method.getName() << " ";
+        std::cout << std::setw(parameterWidth + 2) << std::left << method.getParameters() << std::endl;
     }
     std::cout << "``````````````````````````" << std::endl;
     std::cout << std::endl;
     std::cout << "Fields" << std::endl;
     std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+    scopeWidth = 0;
+    typeWidth = 0;
+    nameWidth = 0;
     for (Field field: getFields()) {
-        std::cout << field.getScope() << " ";
-        std::cout << field.getType() << " ";
-        std::cout << field.getName() << std::endl;
+        scopeWidth = scopeWidth < field.getScope().length() ? field.getScope().length() : scopeWidth;
+        typeWidth = typeWidth < field.getType().length() ? field.getType().length() : typeWidth;
+        nameWidth = nameWidth < field.getName().length() ? field.getName().length() : nameWidth;
+    }
+    for (Field field: getFields()) {
+        std::cout << std::setw(scopeWidth + 2) << std::left << field.getScope() << " ";
+        std::cout << std::setw(typeWidth + 2) << std::left << field.getType() << " ";
+        std::cout << std::setw(nameWidth + 2) << std::left << field.getName() << std::endl;
     }
     std::cout << "``````````````````````````" << std::endl;
     std::cout << std::endl << std::endl;
