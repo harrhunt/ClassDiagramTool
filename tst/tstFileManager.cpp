431852d4df930b2619ff58b6182a6b19e1db9248
lambpati <lambpati@isu.edu>
2022-03-16 14:07:56 -0600

    Initial testing files

--- tst/tstFileManager.cpp
+++ tst/tstFileManager.cpp
@@ -1,14 +1,17 @@
+///
+/// \Authors Hunter, Davis, Patience
+/// \file tst/tstFileManager.hh
+/// \note A test function to determine the effectiveness of the FileManager class
+///
 #include "gtest/gtest.h"
 #include "FileManager.hh"
 #include <assert.h>
 
+TEST(tstFileManager, readFile) {
+    // NOTE: Need relative path from build/tst if in build/tst
+    EXPECT_EQ(FileManager::readFile("../../tst/tstFile.txt"), "Hello World!");
+}
 
-
-TEST(blaTest, test1) {
-    //arrange
-    //act
-    //assert
-   // EXPECT_EQ (FakeClass::bla (0),  0);
-   // EXPECT_EQ (FakeClass::bla (10), 20);
-   // EXPECT_EQ (FakeClass::bla (50), 100);
+TEST(tstFileManager, getFilePaths) {
+    EXPECT_EQ(FileManager::getFilePaths("../../").at(0), "../../.git/logs/refs/stash");
 }
---
+++ tst/tstMethod.cpp
@@ -0,0 +1,31 @@
+///
+/// \Authors Hunter, Davis, Patience
+/// \file tst/tstMethod.hh
+/// \note A test function to determine if Method is getting and setting values correctly
+///
+#include "gtest/gtest.h"
+#include "Method.hh"
+#include <assert.h>
+
+Method method1("diagramTool", "bool", "private", "int diff");
+
+TEST(tstMethod, setGetName){
+    method1.setName("notTool");
+    EXPECT_NE(method1.getName(), "diagramTool");
+}
+
+TEST(tstMethod, setGetType){
+    method1.setType("double");
+    EXPECT_NE(method1.getType(), "bool");
+}
+
+TEST(tstMethod, setGetScope){
+    method1.setScope("public");
+    EXPECT_NE(method1.getScope(), "private");
+}
+
+TEST(tstMethod, setGetParameters){
+    method1.setParameters("long diff");
+    EXPECT_NE(method1.getParameters(), "int diff");
+
+}
